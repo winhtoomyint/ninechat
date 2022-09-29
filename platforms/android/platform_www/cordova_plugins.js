e4bcb1410a6066e0db093e36c54ee114d2d685fa
Henino William <winhtoomyint@gmail.com>
2022-09-29 19:42:55 +0630

    Initial commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-inappbrowser": "5.0.0",
+    "cordova-plugin-whitelist": "1.3.5"
+  };
+});
